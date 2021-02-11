d9c9d0a5ce62455e962ad0f3a66c4eea8356e475
Travja <the.only.t.craft@gmail.com>
2021-02-10 21:06:35 -0700

    Save video (as test.mp4 for now)

--- JavaApp/src/main/java/cameraguys/project/CameraApp.java
+++ JavaApp/src/main/java/cameraguys/project/CameraApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.opencv.core.Core;
 
@@ -28,7 +29,6 @@ public class CameraApp extends Application {
             }
         }
 
-        System.out.println(Core.NATIVE_LIBRARY_NAME);
         System.load(new File("lib", "opencv_java3413.dll").getAbsolutePath());
         System.out.println("Loaded OpenCV");
         launch(args);
@@ -37,18 +37,9 @@ public class CameraApp extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-//        System.out.println("Welcome to OpenCV " + Core.VERSION);
-//        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-//        System.out.println("OpenCV Mat: " + m);
-//        Mat mr1 = m.row(1);
-//        mr1.setTo(new Scalar(1));
-//        Mat mc5 = m.col(5);
-//        mc5.setTo(new Scalar(5));
-//        System.out.println("OpenCV Mat data:\n" + m.dump());
-
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("ClientWindow.fxml"));
-            BorderPane root = loader.load();
+            GridPane root = loader.load();
             ClientWindow controller = loader.getController();
 
             Scene scene = new Scene(root);
--- JavaApp/src/main/java/cameraguys/project/ClientWindow.java
+++ JavaApp/src/main/java/cameraguys/project/ClientWindow.java
@@ -3,6 +3,7 @@ package cameraguys.project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.opencv.core.*;
@@ -20,16 +21,22 @@ import java.util.concurrent.TimeUnit;
 
 public class ClientWindow {
 
+    private static final double MIN_AREA = 50;//Disturbance has to be more than this area to be identified.
     @FXML
     private Button startBtn;
     @FXML
     private ImageView currentFrame, filters;
-
+    @FXML
+    private Slider slider;
     private VideoCapture capture;
     private ScheduledExecutorService timer;
     private boolean cameraActive = false;
+    private Mat diffFrame, displayFrame;
+    private int stage = 0;
+    private boolean outlineSmallerContours = false, outlineAll = true;
+    private VideoWriter writer = null;
 
-    private Mat diffFrame, ret;
+    private long lastMotion = 0l;
 
     public ArrayList<Rect> findContours(Mat outmat) {
         Mat v = new Mat();
@@ -38,79 +45,117 @@ public class ClientWindow {
         Imgproc.findContours(vv, contours, v, Imgproc.RETR_LIST,
                 Imgproc.CHAIN_APPROX_SIMPLE);
 
-        double maxArea = 100;
         int maxAreaIdx;
         Rect r;
-        ArrayList<Rect> rect_array = new ArrayList<Rect>();
+        ArrayList<Rect> rect_array = new ArrayList<>();
+        Rect masterRect;
+        Point min = null;
+        Point max = null;
 
         for (int idx = 0; idx < contours.size(); idx++) {
             Mat contour = contours.get(idx);
             double contourarea = Imgproc.contourArea(contour);
-            if (contourarea > maxArea) {
+            if (contourarea > MIN_AREA) {
                 maxAreaIdx = idx;
                 r = Imgproc.boundingRect(contours.get(maxAreaIdx));
-                rect_array.add(r);
+                if (outlineSmallerContours) {
+                    rect_array.add(r);
+                }
+
+                if (min == null) min = new Point(r.x, r.y);
+                if (max == null) max = new Point(r.x, r.y);
+
+                if (r.x < min.x) min.x = r.x;
+                if (r.y < min.y) min.y = r.y;
+                if (r.x + r.width > max.x) max.x = r.x + r.width;
+                if (r.y + r.height > max.y) max.y = r.y + r.height;
+
                 //This draws outline on the final image with red contours.
 //                Imgproc.drawContours(ret, contours, maxAreaIdx, new Scalar(0, 0, 255));
             }
         }
+        if (min != null && max != null) {
+            masterRect = new Rect((int) min.x, (int) min.y, (int) (max.x - min.x), (int) (max.y - min.y));
+            if (outlineAll)
+                rect_array.add(masterRect);
+        }
 
         v.release();
-
         return rect_array;
 
     }
-    VideoWriter writer = new VideoWriter();
+
+    private void openCamera() {
+        if (!cameraActive) {
+            System.out.println("Attempting to open feed.");
+            cameraActive = capture.open(0, 700);
+            if (cameraActive)
+                System.out.println("Feed opened.");
+            else
+                System.out.println("Could not start camera feed.");
+        }
+    }
 
     @FXML
     protected void startCamera(ActionEvent event) {
         capture = new VideoCapture();
-        Runnable frameGrabber = () -> {
-            if (!cameraActive) {
-                System.out.println("Attempting to open feed.");
-                cameraActive = capture.open(0, 700);
-                if (cameraActive)
-                    System.out.println("Feed opened.");
-                else
-                    System.out.println("Could not start camera feed.");
-            }
+        Runnable frameGrabber = getGrabber();
+        this.timer = Executors.newSingleThreadScheduledExecutor();
+        this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+    }
+
+    private Runnable getGrabber() {
+        return () -> {
+            openCamera();
             if (this.capture.isOpened()) {
-                Mat frame = new Mat();
-                this.capture.read(frame);
-                ret = frame.clone();
-                Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
-                Imgproc.GaussianBlur(frame, frame, new Size(3, 3), 0);
+                Mat initialFrame = new Mat();
+                this.capture.read(initialFrame); //Write the current capture to initialFrame
+                displayFrame = initialFrame.clone(); //Clone it for display purposes.
+                Mat processingFrame = initialFrame.clone(); //This will be changed based on slider to show progress in filters.
 
-                if (diffFrame == null)
-                    diffFrame = frame.clone();
+                Imgproc.cvtColor(initialFrame, initialFrame, Imgproc.COLOR_BGR2GRAY); //Convert to grayscale
+                if (((int) slider.getValue()) == 0) processingFrame = initialFrame.clone();
+                Imgproc.GaussianBlur(initialFrame, initialFrame, new Size(5, 5), 0); //Blur the image a little to de-noise
+                if (((int) slider.getValue()) == 1) processingFrame = initialFrame.clone();
 
-                Mat tempFrame = diffFrame.clone();
-                Core.subtract(frame, tempFrame, diffFrame);
+                if (diffFrame == null) //Initialize the diffFrame if it hasn't already been set.
+                    diffFrame = initialFrame.clone();
+
+                Core.subtract(initialFrame, diffFrame.clone(), diffFrame); //Subtract the previous frame from the current frame.
+                if (((int) slider.getValue()) == 2) processingFrame = diffFrame.clone();
                 Imgproc.adaptiveThreshold(diffFrame, diffFrame, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV, 5, 2);
+                if (((int) slider.getValue()) == 3) processingFrame = diffFrame.clone();
 
                 List<Rect> array = findContours(diffFrame);
-                if (array.size() > 0) {
+                int index = 0;
+                if (array.size() > 0) { // Apply the rectangles to the displayFrame
 
-                    array.forEach(rect -> Imgproc.rectangle(ret, rect.br(), rect.tl(),
-                            new Scalar(0, 255, 0), 1));
+                    for (Rect rect : array) {
+                        boolean isLast = index++ == array.size() - 1;
+                        int thickness = (isLast ? 3 : 1);
+                        Scalar color = isLast ? new Scalar(0, 0, 255) : new Scalar(0, 255, 0);
+                        Imgproc.rectangle(displayFrame, rect.br(), rect.tl(),
+                                color,
+                                thickness);
+                    }
 
+                    processVideo(displayFrame);
                 }
 
+                if (((int) slider.getValue()) == 4) processingFrame = displayFrame.clone();
+
                 MatOfByte buffer = new MatOfByte();
-                Imgcodecs.imencode(".png", ret, buffer);
+                Imgcodecs.imencode(".png", displayFrame, buffer);
                 MatOfByte buf2 = new MatOfByte();
-                Imgcodecs.imencode(".png", diffFrame, buf2);
+                Imgcodecs.imencode(".png", processingFrame, buf2);
 
                 currentFrame.setImage(new Image(new ByteArrayInputStream(buffer.toArray())));
                 filters.setImage(new Image(new ByteArrayInputStream(buf2.toArray())));
-                diffFrame = frame;
+                diffFrame = initialFrame; //Update diffFrame
             } else {
-                System.err.println("Could not start camera feed.");
+                System.err.println("Camera feed not started!");
             }
         };
-        this.timer = Executors.newSingleThreadScheduledExecutor();
-        this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
-
     }
 
     private void stopFrames() {
@@ -125,7 +170,7 @@ public class ClientWindow {
             }
         }
 
-        if (this.capture.isOpened()) {
+        if (this.capture != null && this.capture.isOpened()) {
             // release the camera
             this.capture.release();
         }
@@ -134,6 +179,25 @@ public class ClientWindow {
     }
 
     public void setClosed() {
-        this.stopFrames();
+        stopFrames();
+        stopVideo();
+    }
+
+    private void processVideo(Mat frame) {
+        if (writer == null || !writer.isOpened()) {
+//            int fourcc = VideoWriter.fourcc('M', 'P', '4', 'V');
+            int fourcc = VideoWriter.fourcc('H', '2', '6', '4');
+            writer = new VideoWriter("test.mp4", fourcc, 1000d / 33d, frame.size());
+        }
+
+        if (writer.isOpened())
+            writer.write(frame);
+        else {
+            System.err.println("Could not write video.");
+        }
+    }
+
+    private void stopVideo() {
+        writer.release();
     }
 }
