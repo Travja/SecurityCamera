7a6cf62d5c471658e0c718a76318640a5251bc42
Yaksh Patel <yaksh3636@gmail.com>
2021-02-17 22:53:59 -0700

    Server creation and implemented event listeners to pass data with JSON encoding.

--- NodeApp/server/index.mjs
+++ NodeApp/server/index.mjs
@@ -15,9 +15,11 @@ const { ConfigRoutes } = routes;
 //Engine IO initialize
 const Rooms = require("engine.io-rooms");
 const engine = require("engine.io");
-const port = 5000;
 const http = require("http").createServer(app)
 var server = engine.attach(http);
+
+// Add rooms functionality
+server = Rooms(server);
 
 // declare port
 const PORT = ENV_PORT || 42069;
@@ -40,6 +42,97 @@ if (NODE_ENV === "production") {
   app.get("*", (req, res) => {
     return res.sendFile(path.join(__dirname, "client/index.html"));
   });
+}
+let broadcaster;
+
+server.on("error", (e) => console.log(e));
+server.on("connection", (socket) => {
+  console.log(`${getTime()} Connection: ${socket.id}`);
+
+  socket.on("error", (e) => console.log(e));
+  socket.on("close", () => {
+    console.log("closing connection");
+  });
+
+  socket.on("message", (data) => {
+    console.log(data);
+    let dataObj = JSON.parse(data);
+    console.log("Event: " + dataObj["event"]);
+
+    let exData, id, message;
+
+    switch (data["event"]) {
+      case "broadcaster":
+        console.log(`${getTime()} Broadcaster: ${socket.id}`);
+        broadcaster = socket.id;
+        socket.join(broadcaster);
+        socket.send(prepareData("broadcaster",null));
+        break;
+
+      case "watcher":
+        console.log(`${getTime()} Watcher: ${socket.id}`);
+        exData = {
+          "id":socket.id
+        }
+        socket.room(broadcaster).send(prepareData("watcher",exData));
+        break;
+
+      case "offer":
+        console.log(`${getTime()} Offer: ${socket.id}`);
+        id = data["id"];
+        message = data["message"];
+        exData = {
+          "id": socket.id,
+          "message":message
+        }
+        socket.join(id);
+        socket.room(id).send(prepareData("offer",exData));
+        break;
+
+      case "answer":
+        console.log(`${getTime()} Answer: ${socket.id}`);
+        id = data["id"];
+        message = data["message"];
+        exData = {
+          "id": socket.id,
+          "message":message
+        }
+        socket.join(id);
+        socket.room(id).send(prepareData("answer",exData));
+        break;
+      case "candidate":
+        console.log(`${getTime()} Candidate: ${socket.id}`);
+        id = data["id"];
+        message = data["message"];
+        exData = {
+          "id": socket.id,
+          "message":message
+        }
+        socket.join(id);
+        socket.room(id).send(prepareData("candidate",exData));
+        break;
+      case "disconnect":
+        console.log(`${getTime()} Disconnect: ${socket.id}`);
+        exData = {
+          "id":socket.id
+        }
+        socket.room(broadcaster).send(prepareData("watcher",exData));
+        break;
+    }
+  });
+});
+
+let prepareData = (event, data) => {
+  let encodedData = {
+    event: event,
+  };
+  encodedData = { ...encodedData, ...data };
+  return JSON.stringify(encodedData);
+};
+
+function getTime() {
+  let date = new Date();
+  return `${date.getMinutes()}:${date.getSeconds()}`;
 }
 
 // start to listen
