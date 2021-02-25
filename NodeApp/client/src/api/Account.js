b8a1ce785764c5aab99a03f925e5674cf4f9c3cd
Carter Cobb <carter.cobb72@gmail.com>
2021-02-25 14:02:30 -0700

    protected routes

--- NodeApp/client/src/api/Account.js
+++ NodeApp/client/src/api/Account.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { RT_VAR, T_VAR, ls } from "../redux/redux-reducer";
+import { getNewTokens, handleError } from "./helpers/functions";
 
 class AccountAPI {
   /**
@@ -20,6 +21,41 @@ class AccountAPI {
       }
     } catch (err) {
       cb({ error: err.response ? err.response.data.error : err.message });
+    }
+  }
+
+  /**
+   * Gets the user account from the database
+   * using the stored JWT tokens
+   * @param {Function} cb (err, account)
+   */
+  static async getAccount(cb) {
+    try {
+      const savedToken = await ls.get(T_VAR);
+      const account = await axios.get("/api/account", {
+        headers: { Authorization: `Bearer ${savedToken}` },
+      });
+      if (account.data.error) return cb({ error: account.data.error }, null);
+      return cb(null, account.data.user);
+    } catch (err) {
+      try {
+        const newTokens = await getNewTokens(cb);
+        ls.set(T_VAR, newTokens.token);
+        ls.set(RT_VAR, newTokens.refreshToken);
+        try {
+          const savedToken = await ls.get(T_VAR);
+          const account = await axios.get("/account", {
+            headers: { Authorization: `Bearer ${savedToken}` },
+          });
+          if (account.data.error)
+            return cb({ error: account.data.error }, null);
+          return cb(null, account.data.user);
+        } catch (err2) {
+          handleError(err2, cb);
+        }
+      } catch (err1) {
+        handleError(err1, cb);
+      }
     }
   }
 }
---
+++ NodeApp/client/src/components/ProtectedRoute.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { Route, Redirect } from "react-router-dom";
+import AccountAPI from "../api/Account";
+
+/**
+ * Protects routes lo logged in users only.
+ * ** ALSO tags the user object to the `props` of the autheticated routes.
+ * @param {Object} param0 any react-router-dom `Route` props
+ */
+export const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    let mounted = true;
+    if (mounted)
+      AccountAPI.getAccount((err, account) => {
+        if (err) setUser(false);
+        else setUser(account);
+      });
+    return () => (mounted = false);
+  }, []);
+
+  if (user === null) {
+    return (
+      <div>
+        <h3>Authenticating user...</h3>
+      </div>
+    );
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (typeof user === "object")
+          return <Component {...rest} user={user} />;
+        else
+          return (
+            <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+          );
+      }}
+    />
+  );
+};
