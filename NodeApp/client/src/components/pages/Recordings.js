3915a9eb8f2295e030790d0f9f709d9e0cadf070
Carter Cobb <carter.cobb72@gmail.com>
2021-02-25 00:34:23 -0700

    Updated to use Recording methods

--- NodeApp/client/src/components/pages/Recordings.js
+++ NodeApp/client/src/components/pages/Recordings.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { RecordingCard } from "../RecordingCard";
-import axios from "axios";
 import { connect } from "react-redux";
+import RecordingAPI from "../../api/Recordings";
 
 class Recordings extends Component {
   constructor(props) {
     super(props);
     this.state = {
       recordings: [],
+      loading: true,
     };
 
     this.getRecordings = this.getRecordings.bind();
@@ -23,36 +24,58 @@ class Recordings extends Component {
   }
 
   async getRecordings() {
-    return await (await axios.get("/api/recordings")).data.map((recording) => {
-      return (
-        <RecordingCard
-          title={new Date().toString()}
-          download={recording.url}
-          save={recording.title}
-        >
-          <img src={recording.thumbnail} width="100%" height="100%" />
-        </RecordingCard>
-      );
+    RecordingAPI.getRecordings((err, recordings) => {
+      if (err)
+        this.setState({ loading: false }, () =>
+          console.log("Failed to get recordings", err.error)
+        );
+      else
+        this.setState({
+          loading: false,
+          recordings: recordings.map((recording) => {
+            return (
+              <RecordingCard
+                title={new Date().toString()}
+                download={recording.url}
+                save={recording.title}
+              >
+                <img src={recording.thumbnail} width="100%" height="100%" />
+              </RecordingCard>
+            );
+          }),
+        });
     });
   }
 
   render() {
+    const { loading, recordings } = this.state;
+
     return (
       <div className="page">
         <header>
           <h3>Recordings</h3>
         </header>
         <article>
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr 1fr",
-              justifyItems: "center",
-              gap: "45px",
-            }}
-          >
-            {this.recordings}
-          </div>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <Fragment>
+              {recordings.length === 0 ? (
+                <h4>No Recordings</h4>
+              ) : (
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "1fr 1fr 1fr",
+                    justifyItems: "center",
+                    gap: "45px",
+                  }}
+                >
+                  {this.recordings}
+                </div>
+              )}
+            </Fragment>
+          )}
         </article>
       </div>
     );
