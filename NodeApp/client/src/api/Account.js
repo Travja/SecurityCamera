69eadd69bf1d5d508305557fe4100809822f2988
Carter Cobb <carter.cobb72@gmail.com>
2021-02-24 23:54:23 -0700

    Client login methods

---
+++ NodeApp/client/src/api/Account.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { RT_VAR, T_VAR, ls } from "../redux/redux-reducer";
+
+class AccountAPI {
+  /**
+   * Logs into the app.
+   * @param {Object} payload  {email: String, password: String}
+   * @param {Function} cb (err)
+   */
+  static async login(payload, cb) {
+    try {
+      const login = await axios.post("/api/login", JSON.stringify(payload), {
+        headers: { "Contnet-Type": "application/json" },
+      });
+      if (login.data.error) cb({ error: login.data.error });
+      else {
+        ls.set(T_VAR, login.data.token);
+        ls.set(RT_VAR, login.data.refreshToken);
+        cb(null);
+      }
+    } catch (err) {
+      cb({ error: err.response ? err.response.data.error : err.message });
+    }
+  }
+}
+
+export default AccountAPI;
