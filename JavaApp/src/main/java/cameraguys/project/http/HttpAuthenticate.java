9b0896b9ff5eaaee1827a1609e401214b2c6c435
Travja <the.only.t.craft@gmail.com>
2021-03-05 17:26:41 -0700

    Authenticate Java App

---
+++ JavaApp/src/main/java/cameraguys/project/http/HttpAuthenticate.java
@@ -0,0 +1,71 @@
+package cameraguys.project.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+@AllArgsConstructor
+public class HttpFileUpload {
+
+    @Getter
+    @Setter
+    private String url;
+    @Getter
+    @Setter
+    private File file;
+    @Getter
+    @Setter
+    private HashMap<String, String> formData;
+
+    /**
+     * Sends an email to the current user with the attached file.
+     */
+    private void okSendRequest() {
+        ConnectionInformation info = ConnectionInformation.load();
+
+        if (!url.startsWith("http"))
+            url = "http://" + url;
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+
+        formData.keySet().forEach(key -> {
+            builder.addFormDataPart(key, formData.get(key))
+                    .addFormDataPart("name", info.getName())
+                    .addFormDataPart("userEmail", info.getEmail());
+        });
+
+        builder.addFormDataPart("path", file.getAbsolutePath(),
+                RequestBody.create(MediaType.parse("application/octet-stream"),
+                        file));
+        MultipartBody body = builder.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                file.delete(); //Cleanup!
+            } else {
+                System.err.println("Email notification failed to send. Image will not be deleted.");
+            }
+        } catch (IOException e) {
+            System.err.println("Email notification failed to send. Image will not be deleted.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Uploads the image and sends it in an email to the user.
+     */
+    public void uploadImage() {
+        okSendRequest();
+    }
+
+}
