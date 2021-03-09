68eb49f6c4cd70ecc19e7d059fad16e2ede1c382
CarterWilde <carterjwilde@gmail.com>
2021-03-08 22:52:51 -0700

    Bug Fix

--- NodeApp/server/routes/account-routes.js
+++ NodeApp/server/routes/account-routes.js
@@ -64,7 +64,9 @@ const account_routes = [
                 try {
                     const { email, password } = req.body;
                     let request = await (await useSql()).request();
-                    const result = await request.query`select * from [User] where Email = ${email}`;
+                    console.log(email);
+                    const result = await request.query`select * from [User] where LOWER(Email) = LOWER(${email})`;
+                    console.log(result);
                     if (!result.recordset[0])
                         return res.status(404).json({ error: "No user found" });
                     if (!bcrypt.compareSync(password, result.recordset[0].Hash))
